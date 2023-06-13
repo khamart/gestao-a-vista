2c84d9401eaf562adf83e0314193c4c4e1c0cba2
khamart <121980281+khamart@users.noreply.github.com>
2023-06-13 10:50:36 -0300

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,801 @@
+var json = [
+{
+"Index": "sinal1",
+"Tipo": "PRR",
+"TAG": "01.001",
+"Equipamento": "30006936",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal2",
+"Tipo": "PRR",
+"TAG": "11.001",
+"Equipamento": "30006938",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal3",
+"Tipo": "PRR",
+"TAG": "15.001",
+"Equipamento": "30006939",
+"Status": "2",
+"Color": "yellow"
+}, 
+{
+"Index": "sinal4",
+"Tipo": "PRR",
+"TAG": "15.002",
+"Equipamento": "30006942",
+"Status": "2",
+"Color": "yellow"
+}, 
+{
+"Index": "sinal5",
+"Tipo": "PRR",
+"TAG": "15.003",
+"Equipamento": "30006943",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal6",
+"Tipo": "PRR",
+"TAG": "16.001",
+"Equipamento": "30006947",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal7",
+"Tipo": "PRR",
+"TAG": "28.001",
+"Equipamento": "30006948",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal8",
+"Tipo": "PRR",
+"TAG": "28.002",
+"Equipamento": "30006949",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal9",
+"Tipo": "PRR",
+"TAG": "31.001",
+"Equipamento": "30006952",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal10",
+"Tipo": "PRR",
+"TAG": "31.002",
+"Equipamento": "30006954",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal11",
+"Tipo": "PRR",
+"TAG": "31.003",
+"Equipamento": "30006955",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal12",
+"Tipo": "PRR",
+"TAG": "31.004",
+"Equipamento": "30006956",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal13",
+"Tipo": "PRR",
+"TAG": "31.005",
+"Equipamento": "30006957",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal14",
+"Tipo": "PRR",
+"TAG": "31.006",
+"Equipamento": "30006958",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal15",
+"Tipo": "PRR",
+"TAG": "31.007",
+"Equipamento": "30006959",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal16",
+"Tipo": "PRR",
+"TAG": "31.008",
+"Equipamento": "30009405",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal17",
+"Tipo": "PRR",
+"TAG": "31.009",
+"Equipamento": "30009761",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal18",
+"Tipo": "PRR",
+"TAG": "31.010",
+"Equipamento": "30009762",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal19",
+"Tipo": "PRR",
+"TAG": "31.011",
+"Equipamento": "30010020",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal20",
+"Tipo": "PRR",
+"TAG": "43.001",
+"Equipamento": "30006975",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal21",
+"Tipo": "PRR",
+"TAG": "43.002",
+"Equipamento": "30006976",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal22",
+"Tipo": "PRR",
+"TAG": "43.003",
+"Equipamento": "30006977",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal23",
+"Tipo": "PRR",
+"TAG": "50.001",
+"Equipamento": "30007468",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal24",
+"Tipo": "PRR",
+"TAG": "50.002",
+"Equipamento": "30007469",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal25",
+"Tipo": "PRR",
+"TAG": "50.003",
+"Equipamento": "30007470",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal26",
+"Tipo": "PRR",
+"TAG": "50.004",
+"Equipamento": "30007471",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal27",
+"Tipo": "PRR",
+"TAG": "50.005",
+"Equipamento": "30007472",
+"Status": "2",
+"Color": "yellow"
+}, 
+{
+"Index": "sinal28",
+"Tipo": "PRR",
+"TAG": "50.006",
+"Equipamento": "30007473",
+"Status": "2",
+"Color": "yellow"
+}, 
+{
+"Index": "sinal29",
+"Tipo": "PRR",
+"TAG": "50.007",
+"Equipamento": "30007474",
+"Status": "2",
+"Color": "yellow"
+}, 
+{
+"Index": "sinal30",
+"Tipo": "PRR",
+"TAG": "50.008",
+"Equipamento": "30007475",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal31",
+"Tipo": "PRR",
+"TAG": "50.009",
+"Equipamento": "30007476",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal32",
+"Tipo": "PRR",
+"TAG": "50.010",
+"Equipamento": "30007477",
+"Status": "2",
+"Color": "yellow"
+}, 
+{
+"Index": "sinal33",
+"Tipo": "PRR",
+"TAG": "50.011",
+"Equipamento": "30007478",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal34",
+"Tipo": "PRR",
+"TAG": "50.012",
+"Equipamento": "30007479",
+"Status": "2",
+"Color": "yellow"
+}, 
+{
+"Index": "sinal35",
+"Tipo": "PRR",
+"TAG": "50.013",
+"Equipamento": "30007480",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal36",
+"Tipo": "PRR",
+"TAG": "50.014",
+"Equipamento": "30007481",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal37",
+"Tipo": "PRR",
+"TAG": "50.015",
+"Equipamento": "30007482",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal38",
+"Tipo": "PRR",
+"TAG": "50.016",
+"Equipamento": "30007483",
+"Status": "2",
+"Color": "yellow"
+}, 
+{
+"Index": "sinal39",
+"Tipo": "PRR",
+"TAG": "52,001",
+"Equipamento": "30010205",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "sinal40",
+"Tipo": "PRR",
+"TAG": "52,002",
+"Equipamento": "30010206",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao1",
+"Tipo": "PRS",
+"TAG": "15,001",
+"Equipamento": "30006984",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao2",
+"Tipo": "PRS",
+"TAG": "15.002",
+"Equipamento": "30006985",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao3",
+"Tipo": "PRS",
+"TAG": "15.003",
+"Equipamento": "30006986",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao4",
+"Tipo": "PRS",
+"TAG": "15.004",
+"Equipamento": "30006987",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao5",
+"Tipo": "PRS",
+"TAG": "15.005",
+"Equipamento": "30006988",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao6",
+"Tipo": "PRS",
+"TAG": "15.006",
+"Equipamento": "30009764",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao7",
+"Tipo": "PRS",
+"TAG": "43.001",
+"Equipamento": "30006989",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao8",
+"Tipo": "PRS",
+"TAG": "43.002",
+"Equipamento": "30007004",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao9",
+"Tipo": "PRS",
+"TAG": "43.003",
+"Equipamento": "30007005",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao10",
+"Tipo": "PRS",
+"TAG": "43.004",
+"Equipamento": "30007006",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao11",
+"Tipo": "PRS",
+"TAG": "43.005",
+"Equipamento": "30007007",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao12",
+"Tipo": "PRS",
+"TAG": "43.006",
+"Equipamento": "30007008",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao13",
+"Tipo": "PRS",
+"TAG": "43.007",
+"Equipamento": "30007009",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao14",
+"Tipo": "PRS",
+"TAG": "43.008",
+"Equipamento": "30007010",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao15",
+"Tipo": "PRS",
+"TAG": "43.009",
+"Equipamento": "30007011",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao16",
+"Tipo": "PRS",
+"TAG": "43.010",
+"Equipamento": "30007012",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao17",
+"Tipo": "PRS",
+"TAG": "43.011",
+"Equipamento": "30007013",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao18",
+"Tipo": "PRS",
+"TAG": "43.012",
+"Equipamento": "30007014",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao19",
+"Tipo": "PRS",
+"TAG": "43.013",
+"Equipamento": "30007015",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao20",
+"Tipo": "PRS",
+"TAG": "43.014",
+"Equipamento": "30007016",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao21",
+"Tipo": "PRS",
+"TAG": "43.015",
+"Equipamento": "30007017",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao22",
+"Tipo": "PRS",
+"TAG": "43.016",
+"Equipamento": "30007018",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao23",
+"Tipo": "PRS",
+"TAG": "43.017",
+"Equipamento": "30007019",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao24",
+"Tipo": "PRS",
+"TAG": "43.018",
+"Equipamento": "30007020",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao25",
+"Tipo": "PRS",
+"TAG": "43.019",
+"Equipamento": "30007021",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao26",
+"Tipo": "PRS",
+"TAG": "43.020",
+"Equipamento": "30007022",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao27",
+"Tipo": "PRS",
+"TAG": "43.021",
+"Equipamento": "30007023",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao28",
+"Tipo": "PRS",
+"TAG": "43.022",
+"Equipamento": "30007024",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao29",
+"Tipo": "PRS",
+"TAG": "43.023",
+"Equipamento": "30007025",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "secao30",
+"Tipo": "PRS",
+"TAG": "49.001",
+"Equipamento": "30007026",
+"Status": "3",
+"Color": "green"
+}, 
+{
+"Index": "gota1",
+"Tipo": "GOT",
+"TAG": "P01",
+"Equipamento": "30007039",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota2",
+"Tipo": "GOT",
+"TAG": "P02",
+"Equipamento": "30007040",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota3",
+"Tipo": "GOT",
+"TAG": "P03",
+"Equipamento": "30007041",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota4",
+"Tipo": "GOT",
+"TAG": "P04",
+"Equipamento": "30007042",
+"Status": "1",
+"Color": "blue"
+}, 
+{
+"Index": "gota5",
+"Tipo": "GOT",
+"TAG": "P07",
+"Equipamento": "30007043",
+"Status": "2",
+"Color": "#22d4fd"
+}, 
+{
+"Index": "gota6",
+"Tipo": "GOT",
+"TAG": "P08",
+"Equipamento": "30007044",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota7",
+"Tipo": "GOT",
+"TAG": "P11",
+"Equipamento": "30007045",
+"Status": "2",
+"Color": "#22d4fd"
+}, 
+{
+"Index": "gota8",
+"Tipo": "GOT",
+"TAG": "P15",
+"Equipamento": "30007046",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota9",
+"Tipo": "GOT",
+"TAG": "P16",
+"Equipamento": "30007047",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota10",
+"Tipo": "GOT",
+"TAG": "P18",
+"Equipamento": "30007048",
+"Status": "2",
+"Color": "#22d4fd"
+}, 
+{
+"Index": "gota11",
+"Tipo": "GOT",
+"TAG": "P20",
+"Equipamento": "30007049",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota12",
+"Tipo": "GOT",
+"TAG": "P22",
+"Equipamento": "30007050",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota13",
+"Tipo": "GOT",
+"TAG": "P26",
+"Equipamento": "30007051",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota14",
+"Tipo": "GOT",
+"TAG": "P27",
+"Equipamento": "30007052",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota15",
+"Tipo": "GOT",
+"TAG": "P28",
+"Equipamento": "30007053",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota16",
+"Tipo": "GOT",
+"TAG": "P29",
+"Equipamento": "30007054",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota17",
+"Tipo": "GOT",
+"TAG": "P30",
+"Equipamento": "30007055",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota18",
+"Tipo": "GOT",
+"TAG": "P31",
+"Equipamento": "30007056",
+"Status": "2",
+"Color": "#22d4fd"
+}, 
+{
+"Index": "gota19",
+"Tipo": "GOT",
+"TAG": "P32",
+"Equipamento": "30007057",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota20",
+"Tipo": "GOT",
+"TAG": "P35",
+"Equipamento": "30007058",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota21",
+"Tipo": "GOT",
+"TAG": "P37",
+"Equipamento": "30009722",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota22",
+"Tipo": "GOT",
+"TAG": "P38",
+"Equipamento": "30007059",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota23",
+"Tipo": "GOT",
+"TAG": "P40",
+"Equipamento": "30007060",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota24",
+"Tipo": "GOT",
+"TAG": "P42",
+"Equipamento": "30007061",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota25",
+"Tipo": "GOT",
+"TAG": "P43",
+"Equipamento": "30007062",
+"Status": "2",
+"Color": "#22d4fd"
+}, 
+{
+"Index": "gota26",
+"Tipo": "GOT",
+"TAG": "P48",
+"Equipamento": "30007065",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota27",
+"Tipo": "GOT",
+"TAG": "P49",
+"Equipamento": "30007066",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota28",
+"Tipo": "GOT",
+"TAG": "P50",
+"Equipamento": "30009721",
+"Status": "1",
+"Color": "blue"
+}, 
+{
+"Index": "gota29",
+"Tipo": "GOT",
+"TAG": "P52",
+"Equipamento": "30010383",
+"Status": "3",
+"Color": "transparent"
+}, 
+{
+"Index": "gota30",
+"Tipo": "GOT",
+"TAG": "P56",
+"Equipamento": "30010384",
+"Status": "3",
+"Color": "transparent"
+}];
